970e81dcbf1d6bc3bebae95cf3ea86e683a618ca
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2023-05-24 17:15:18 -0500

    Added the bg photo to the projects page and continued styling it. Need to figure out how to get the project place holders to be placed evenly on the screen and to wrap when small screen.

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,42 @@
 import React from "react";
+import projectPagePhoto from "../images/breathtaking-scenery-milky-way-italian-alps.jpg";
 
 const Home = () => {
   return (
-    <>
-      <div className="flex flex-wrap flex-row justify-center ">
-        <div className="w-64 h-32 bg-blue-300 m-5"></div>
-        <div className="w-64 h-32 bg-blue-300 m-5"></div>
-        <div className="w-64 h-32 bg-blue-300 m-5"></div>
-        <div className="w-64 h-32 bg-blue-300 m-5"></div>
-        <div className="w-64 h-32 bg-blue-300 m-5"> </div>
-        <div className="w-64 h-32 bg-blue-300 m-5"></div>
+    <div className="relative">
+      <img className="max-lg:h-[75rem]" style={{ height: "1250px", width: "100%" }} src={projectPagePhoto} />
+
+      <div className=" absolute top-7 mt-4 w-fullflex flex-wrap flex-row justify-center ">
+        <div className="flex justify-between align-middle max-lg:flex-col max-lg:justify-center">
+          <div className="flex flex-col items-center justify-center pr-10 max-lg:pr-0 max-lg:pb-20">
+            <div className="w-64 h-32 bg-blue-300 m-5"></div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center pr-10 max-lg:pr-0 max-lg:pb-20">
+            <div className="w-64 h-32 bg-blue-300 m-5"></div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center pr-10 max-lg:pr-0 max-lg:pb-20">
+            <div className="w-64 h-32 bg-blue-300 m-5"></div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center pr-10 max-lg:pr-0 max-lg:pb-20">
+            <div className="w-64 h-32 bg-blue-300 m-5"></div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center pr-10 max-lg:pr-0 max-lg:pb-20">
+            <div className="w-64 h-32 bg-blue-300 m-5"></div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center pr-10 max-lg:pr-0 max-lg:pb-20">
+            <div className="w-64 h-32 bg-blue-300 m-5"></div>
+          </div>
+        </div>
       </div>
-    </>
+      <div className=" absolute bottom-4 text-white">
+        <h3>Image by wirestock on Freepik</h3>
+      </div>
+    </div>
   );
 };
 
