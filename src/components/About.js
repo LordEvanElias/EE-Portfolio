b15eac7f12b28e083663e08d104832757d7ef330
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2023-06-02 10:50:02 -0500

    Fixed the location of the about card on the page. Need to style and place photo and text.

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,14 @@ import placeHolder from "../images/placeHolder.png";
 const About = () => {
   return (
     <div className="relative flex justify-center">
-      <div className="absolute top-7">
-        <img className="max-lg:h-[75rem]" style={{ height: "1250px", width: "100%" }} src={projectPagePhoto} />
+      <img className="max-lg:h-[75rem]" style={{ height: "1250px", width: "100%" }} src={projectPagePhoto} />
+      <div className="absolute top-7 mt-4 w-fit ">
+        <div className=" flex flex-row border border-solid border-white bg-blue-200">
+          <img className="max-lg:h-[75rem] mt-5 mb-5 ml-5" style={{ height: "640px", width: "360px" }} src={placeHolder} />
 
-        <div className=" justify-center">
-          <img className="max-lg:h-[75rem]" style={{ height: "640px", width: "360px" }} src={placeHolder} />
-          <h1>Here is text</h1>
+          <div className=" border border-solid border-blue-500">
+            <h1>Here is text</h1>
+          </div>
         </div>
       </div>
     </div>
