bb86d5067ca8f53ffc79a7e8ef6a8dc38d87556d
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2023-06-04 13:51:48 -0500

    Finished the About page frame. Made mobile friendly. Need to finish the photo and description and done. Need to finish projects and resume pages.

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,10 @@ const About = () => {
     <div className="relative flex justify-center">
       <img className="max-lg:h-[75rem]" style={{ height: "1250px", width: "100%" }} src={projectPagePhoto} />
       <div className="absolute top-7 mt-4 w-1/2 ">
-        <div className=" flex flex-row max-lg:flex-col border border-solid border-white bg-blue-200">
-          <img className="max-lg:items-center" style={{ height: "360px", width: "360px" }} src={placeHolder} />
+        <div className=" flex flex-row max-lg:flex-col max-lg:items-center border border-solid border-white backdrop-blur-sm">
+          <img style={{ height: "360px", width: "360px" }} src={placeHolder} />
 
-          <div className="border border-solid border-blue-500 text-center flex items-center w-full mt-20 mb-20 align-middle">
+          <div className="max-lg:pt-3 max-lg:pb-3 border border-transparent  text-center text-white p-3 font-bold flex items-center w-full  align-middle">
             <h1>Hello, my name is Evan Elias. I am a bilingual Full Stack Developer. I am interested in a career working for companies that help the world in some way. </h1>
           </div>
         </div>
