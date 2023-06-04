042ac6f33407a650e297ceea751429d319d6dcb3
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2023-06-03 21:44:13 -0500

    Fixed the basic card for the about page.

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,18 @@
 import React from "react";
 import projectPagePhoto from "../images/breathtaking-scenery-milky-way-italian-alps.jpg";
 import placeHolder from "../images/placeHolder.png";
+import "./About.module.css";
 
 const About = () => {
   return (
     <div className="relative flex justify-center">
       <img className="max-lg:h-[75rem]" style={{ height: "1250px", width: "100%" }} src={projectPagePhoto} />
-      <div className="absolute top-7 mt-4 w-fit ">
-        <div className=" flex flex-row border border-solid border-white bg-blue-200">
-          <img className="max-lg:h-[75rem] mt-5 mb-5 ml-5" style={{ height: "640px", width: "360px" }} src={placeHolder} />
+      <div className="absolute top-7 mt-4 w-1/2 ">
+        <div className=" flex flex-row max-lg:flex-col border border-solid border-white bg-blue-200">
+          <img className="max-lg:items-center" style={{ height: "360px", width: "360px" }} src={placeHolder} />
 
-          <div className=" border border-solid border-blue-500">
-            <h1>Here is text</h1>
+          <div className="border border-solid border-blue-500 text-center flex items-center w-full mt-20 mb-20 align-middle">
+            <h1>Hello, my name is Evan Elias. I am a bilingual Full Stack Developer. I am interested in a career working for companies that help the world in some way. </h1>
           </div>
         </div>
       </div>
