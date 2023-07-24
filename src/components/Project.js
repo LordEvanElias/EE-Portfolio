76ec970cef2838cc601f527301475b6b6e312d99
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2023-07-24 16:17:58 -0500

    Finally fixed the bug that didn't allow the prop to access the images.

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,43 +6,21 @@ import ProjectImage from "./ProjectImage";
 
 const Project = () => {
   return (
-    <div className="relative flex justify-center ">
+    <div className="relative flex flex-wrap justify-around">
       <img className="max-lg:h-[75rem]" style={{ height: "1250px", width: "100%" }} src={projectPagePhoto} />
 
-      <ProjectImage img={OceanApp} />
+      
+        <ProjectImage img={OceanApp} />
+        <ProjectImage img={Bikes4u} />
+      
 
-      <div className=" absolute top-7 mt-4 ">
-        <div className="flex flex-wrap max-lg:flex-col justify-around ">
-          <div className="p-2">
-            <img className="max-lg:h-[25rem]" style={{ height: "16rem", width: "32rem" }} src={OceanApp} />
-          </div>
-
-          <div className="p-2">
-            <img className="max-lg:h-[25rem]" style={{ height: "16rem", width: "32rem" }} src={Bikes4u} />
-          </div>
-
-          <div className="p-2">
-            <img className="max-lg:h-[25rem]" style={{ height: "16rem", width: "32rem" }} src={OceanApp} />
-          </div>
-
-          <div className="p-2">
-            <img className="max-lg:h-[25rem]" style={{ height: "16rem", width: "32rem" }} src={Bikes4u} />
-          </div>
-
-          <div className="p-2">
-            <img className="max-lg:h-[25rem]" style={{ height: "16rem", width: "32rem" }} src={OceanApp} />
-          </div>
-
-          <div className="p-2">
-            <img className="max-lg:h-[25rem]" style={{ height: "16rem", width: "32rem" }} src={Bikes4u} />
-          </div>
-        </div>
-      </div>
       <div className=" absolute bottom-4 text-white">
         <h3>Image by wirestock on Freepik</h3>
       </div>
     </div>
   );
 };
+
+
 
 export default Project;
--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -2,13 +2,13 @@ import React from "react";
 
 const ProjectImage = (props) => {
   return (
-    <div className="absolute text-white">
+    <div className="static flex text-white bg-sky-950 items-center pl-4" style={{ height: "18rem", width: "34rem" }}>
       {/* Base Project Image */}
-      <img className="project-image" style={{ height: "16rem", width: "32rem" }} src={props.image} alt="image" />
+      <img className="img-source flex  " style={{ height: "16rem", width: "32rem" }} src={props.img} alt="" />
       {/* Glass Overlay */}
       <div className="hover:backdrop-blur-md">
         {/* Github and website Link */}
-        <div className=""></div>
+        <div className="git-web-link"></div>
       </div>
     </div>
   );
